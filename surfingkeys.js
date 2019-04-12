20705cf45d4e375af7a4fedd00948c75ddf8f125
KhaosChow <khaos.chow@gmail.com>
2019-04-12 13:34:45 +0800

    ..

--- surfingkeys.js
+++ surfingkeys.js
@@ -1,7 +1,3 @@
-mapkey('gR', '#12重启Chrome', function() {
-        tabOpenLink("chrome://restart/");
-    });
-
 mapkey('zf', '#1在新标签页后台打开多个链接', function() {
     Hints.create("", Hints.dispatchMouseClick, {tabbed: true, active: false, multipleHits: true});
 });
